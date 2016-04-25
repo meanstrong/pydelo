ca7a5d3902f11c8dafd45fe3180f5147c3b3cf7a
rocky <pmq2008@gmail.com>
2016-04-25 14:39:55 +0800

    Fix:init添加user时，密码要计算md5值

--- init.py
+++ init.py
@@ -1,13 +1,14 @@
 import random, string
+from hashlib import md5
 from web.services.users import users
 from web.services.projects import projects
 
 apikey = ''.join(random.choice(string.letters+string.digits) for _ in range(32))
-users.create(name="demo1",
-             password="123456",
+users.create(name="demo",
+             password=md5("123456".encode("utf-8")).hexdigest().upper(),
              apikey=apikey)
-#projects.create(name="pydelo",
-#                repo_url="",
-#                checkout_dir="/data/home/rocky/pydelo/test/checkout",
-#                deploy_dir="/data/home/rocky/pydelo/test/deploy",
-#                deploy_history_dir="/data/home/rocky/pydelo/history")
+projects.create(name="pydelo",
+                repo_url="",
+                checkout_dir="/data/home/rocky/pydelo/test/checkout",
+                deploy_dir="/data/home/rocky/pydelo/test/deploy",
+                deploy_history_dir="/data/home/rocky/pydelo/history")
