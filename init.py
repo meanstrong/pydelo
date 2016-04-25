4b3c2bc8fc4c60f9d2ae7975028ea7f693a011ad
rocky <pmq2008@gmail.com>
2016-04-25 15:56:48 +0800

    Fix:init时root和demo指定不同的apikey

--- init.py
+++ init.py
@@ -3,14 +3,13 @@ from hashlib import md5
 from web.services.users import users
 from web.services.projects import projects
 
-apikey = ''.join(random.choice(string.letters+string.digits) for _ in range(32))
 users.create(name="root",
              password=md5("123456".encode("utf-8")).hexdigest().upper(),
-             apikey=apikey,
+             apikey=''.join(random.choice(string.letters+string.digits) for _ in range(32)),
              role=1)
 users.create(name="demo",
              password=md5("123456".encode("utf-8")).hexdigest().upper(),
-             apikey=apikey)
+             apikey=''.join(random.choice(string.letters+string.digits) for _ in range(32)))
 projects.create(name="pydelo",
                 repo_url="",
                 checkout_dir="/data/home/rocky/pydelo/test/checkout",
