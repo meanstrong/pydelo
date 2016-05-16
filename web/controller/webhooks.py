bf2e246b0cb65f8ab75e277694a272591374b6db
rocky <pmq2008@gmail.com>
2016-05-16 17:31:12 +0800

    add build action

--- web/controller/webhooks.py
+++ web/controller/webhooks.py
@@ -37,3 +37,28 @@ def webhooks_push_events():
         return jsonify({"rc": 0}), 200
     else:
         return jsonify({"rc": 0}), 204
+
+@app.route('/api/webhooks/build_events', methods=["POST"])
+@authorize
+def webhooks_build_events():
+    project_id = request.args.get("project_id")
+    host_id = request.args.get("host_id")
+    data = request.json
+    branch = data["ref"].split("/", 2)[-1]
+    version = data["after"][:7]
+    # 只针对dev分支进行deploy
+    if data["ref"] == "refs/heads/dev" and data["total_commits_count"] > 0:
+        deploy = deploys.create(
+            user_id=g.user.id,
+            project_id=project_id,
+            host_id=host_id,
+            mode=0,
+            status=2,
+            branch=branch,
+            version=version,
+            softln_filename=time.strftime("%Y%m%d-%H%M%S") + "-" + version,
+            )
+        deploys.build(deploy)
+        return jsonify({"rc": 0}), 200
+    else:
+        return jsonify({"rc": 0}), 204
