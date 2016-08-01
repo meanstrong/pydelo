41d5789132deaa7339838976bb3766064a7b565e
rocky <pmq2008@gmail.com>
2016-08-01 15:49:43 +0800

    update MySQL upgrade

--- db-schema/db-schema-upgrade_v1.0.0_to_v2.0.0.sql
+++ db-schema/db-schema-upgrade_v1.0.0_to_v2.0.0.sql
@@ -1,3 +1,5 @@
 USE pydelo;
 ALTER TABLE `projects` ADD `target_dir` varchar(200) NOT NULL COMMENT '部署文件build后目标目录' AFTER `checkout_dir`;
 UPDATE `projects` SET `target_dir` = `checkout_dir`;
+ALTER TABLE `projects` DROP COLUMN `before_rollback`;
+ALTER TABLE `projects` DROP COLUMN `after_rollback`;
