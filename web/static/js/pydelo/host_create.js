74ba65b4150479ff6a4b8aec3c83401d24022056
meanstrong <pmq2008@gmail.com>
2018-01-15 17:52:18 +0800

    delete ssh_method db-schema and use default PASSWORD

--- web/static/js/pydelo/host_create.js
+++ web/static/js/pydelo/host_create.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#submit").click(function(e){
         create_host(
-            {"name": $("#name").val(), "ssh_host": $("#ssh_host").val(), "ssh_port": $("#ssh_port").val(), "ssh_user": $("#ssh_user").val(), "ssh_pass": $("#ssh_pass").val()},
+            {"name": $("#name").val(), "ssh_host": $("#ssh_host").val(), "ssh_port": $("#ssh_port").val(), "ssh_method": 0, "ssh_user": $("#ssh_user").val(), "ssh_pass": $("#ssh_pass").val()},
             function(data){
                 check_return(data);
                 window.location.assign('/hosts');
